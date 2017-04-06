48d908048e7cf4b55ffda51c6f63e7d843d86790
Dombovari Peter <dombo.peter@gmail.com>
2017-04-06 10:55:42 +0200

    feat(Print usage): print usage in arg handler skeleton

--- src/TodoApp.java
+++ src/TodoApp.java
@@ -1,5 +1,28 @@
 public class TodoApp {
   public static void main(String[] args) {
-    System.out.println("Hello AppWorld!");
+    argHandler(args);
+    }
+
+    public static void argHandler(String[] args) {
+      if (args.length > 0) {
+        switch (args[0]) {
+          case "-l":
+            System.out.println("-l");
+            break;
+          case "-a":
+            System.out.println("-a");
+            break;
+          case "-r":
+            System.out.println("-r");
+            break;
+          case "-c":
+            System.out.println("-c");
+            break;
+        }
+      } else {
+        System.out.println(" \n Java Todo application\n ======================= \n Command line " +
+                "arguments: \n -l   Lists all the tasks \n -a   Adds a new task \n -r   Removes " +
+                "antask \n -c   Completes an task");
+      }
+    }
   }
-}
